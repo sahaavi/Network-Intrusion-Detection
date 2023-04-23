ffe532eb1a5cc963f47688fc473f14291a794dc6
sahaavi <avisheksaha123@gmail.com>
2023-04-23 02:10:12 -0700

    update model and app

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import pandas as pd
 # create app
 app = Flask(__name__)
 # load model
-mlp_model = pickle.load(open('model/mlp.pkl', 'rb'))
-scalar = pickle.load(open('model/scalar.pkl', 'rb'))
+mlp_model = pickle.load(open('model/mlp_sf.pkl', 'rb'))
+scaler = pickle.load(open('model/scaler_sf.pkl', 'rb'))
 
 # create route that renders index.html template
 @app.route("/")
@@ -18,16 +18,16 @@ def home():
 @app.route("/predict_api", methods=['POST'])
 def predict_api():
     # get data
-    data=request.get_json['data']
+    data=request.get_json()['data']
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     # transform data
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
     # predict
-    output = mlp_model.predict(new_data)
+    output = mlp_model.predict(new_data)[0]
     # send back to browser
-    output = output[0]
-    return jsonify(output[0])
+    output = output.tolist()
+    return jsonify(output)
 
 # run app
 if __name__ == "__main__":
